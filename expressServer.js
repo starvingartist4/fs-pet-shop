534ab4f5f242fcb0fb30b8b079ac3a6f1608d94a
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-02-22 16:23:53 -0800

    partThree complete

--- expressServer.js
+++ expressServer.js
@@ -6,7 +6,10 @@ const petsPath = path.join(__dirname, 'pets.json');
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 8000;
+
+app.use(bodyParser.json());
 
 app.get('/pets', (req, res) => {
   fs.readFile(petsPath, 'utf8', function(err, data) {
@@ -32,7 +35,7 @@ app.get('/pets/:id', (req, res) => { // create instance of Express
     let pet = pets[idx]; //target one pet at the idx of the json data
 
     if (idx > pets.length-1 || idx < 0 || Number.isNaN(idx)) { //if that idx is out of range, less than 0, or idx isn't a number...
-      res.set('Content-Type', 'text/plain');
+      res.set('Content-Type', 'text/plain'); //create header for content type
       return res.status(404).send('Not Found'); //return 404 error as response status code
     }
 
@@ -41,7 +44,43 @@ app.get('/pets/:id', (req, res) => { // create instance of Express
   });
 });
 
-app.use(function(req, res) {
+app.post('/pets', (req, res) => {
+  fs.readFile(petsPath, 'utf8', (readErr, data) => {
+    if (readErr) {
+      console.error(readErr.stack);
+      return res.sendStatus(500);
+    }
+
+    let pets = JSON.parse(data);
+    let newPet = {
+      'age': Number.parseInt(req.body.age),
+      'kind': req.body.kind,
+      'name': req.body.name
+    }
+
+    if (!newPet.age || !newPet.kind || !newPet.name || Number.isNaN(newPet.age)) {
+      res.set('Content-Type', 'text/plain');
+      return res.status(400).send('Bad Request');
+    }
+
+    pets.push(newPet);
+
+    let petsJSON = JSON.stringify(pets);
+
+    fs.writeFile(petsPath, petsJSON, (writeErr) => {
+      if (writeErr) {
+        console.error(writeErr.stack);
+        return res.sendStatus(500);
+      }
+
+      res.set('Content-Type', 'application/json');
+      res.status(200).send(newPet);
+    });
+
+  });
+});
+
+app.use( (req, res) => {
   res.sendStatus(404);
 });
 
